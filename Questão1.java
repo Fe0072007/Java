ad4902316e0eec49795dedf3ef9f649f8895c483
Fe0072007 <159498290+Fe0072007@users.noreply.github.com>
2024-05-08 10:14:59 -0300

    Questao1.java

---
+++ Questão1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Questao1{
+    
+    float Media(float n1, float n2, float n3, float n4){
+        
+        float media = (n1 + n2 + n3 + n4) / 4;
+        return media;
+    }
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("");
+		float nota1 = scnr.nextFloat();
+		float nota2 = scnr.nextFloat();
+		float nota3 = scnr.nextFloat();
+		float nota4 = scnr.nextFloat();
+		float m = new Main().Media(nota1,nota2,nota3,nota4);
+		System.out.println(m);
+	}
+}
